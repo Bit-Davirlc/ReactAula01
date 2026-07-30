1888a99c1d013152db7248c0b212b464adbe1f3b
Aluno <e023aluno@outlook.com>
2026-07-30 20:25:31 -0300

    Primeira Aula PWII

---
+++ aula01-react/src/App copy.jsx
@@ -0,0 +1,10 @@
+import Header from "./components/header";
+
+export default function App() {
+  return (
+    <>
+      < Header />
+      <p>'Meu primeiro componente em React'</p>
+    </>
+  );
+}
---
+++ aula01-react/src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header(){
+    return(
+        <header>Bem vindo ao REACT</header>
+    )
+}
